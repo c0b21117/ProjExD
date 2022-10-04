1e5c454364e910a5d0f46ebafe73b424053a58e0
c0b21117 <c0b211173f@edu.teu.ac.jp>
2022-10-04 14:52:39 +0900

    電卓練習３まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,18 +1,26 @@
 from calendar import c
+import numbers
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.geometry("300x500")
 
-r = 0
-c = 0
+def button_click(event):
+    btn = event.widget
+    num = int(btn["text"])
+    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+
+r = 0 #行を表す
+c = 0 #列を表す
 for i, num in enumerate(range(9,-1,-1),1):
     btn = tk.Button(root,
                     text=str(num),
                     font = ("",30),
                     width = 4,
-                    height = 2
+                    height = 2,
                     )
+    btn.bind("<1>", button_click)
     btn.grid(row = r, column = c)
     c += 1
     if i%3 == 0:
